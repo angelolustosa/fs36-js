aa4f6344f9a992adc029e0d70aad3cb2430ea592
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2024-05-16 22:01:09 -0300

    Soma

---
+++ js/inicio/2-tipos_dados.js
@@ -0,0 +1,27 @@
+let n1 = 25,
+    n2 = 15,
+    n3 = 10;
+
+let resutado = n1 + n2 +n3
+//console.log(resutado);
+
+//console.log(n1, n2, n3);
+//console.log(n1, n3, n2);
+//console.log(n1 + n2 + n3);
+
+
+//soma
+let numero1 = 100;
+let numero2 = 500;
+let palavra = 'teste'
+
+// string com o sinal de '+' o js entende como concatenação, ou seja, 
+let resultado = numero1 + numero2
+console.log(resultado)
+
+let resultadoMenos = numero1 - numero2
+console.log(resultadoMenos)
+
+let resultadoPalavra = palavra - numero2
+console.log(resultadoPalavra)
+
---
+++ js/script2.js
@@ -0,0 +1,5 @@
+//console.log('Rodando via head');
+
+let nome = 'Angelo Lustosa de Figueiredo Filho'
+let teste = nome.replace(' ', '')
+console.log(teste);
