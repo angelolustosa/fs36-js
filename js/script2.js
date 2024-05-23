cff28a68bd4f93f3c718397f250fdd68bee9f671
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2024-05-23 20:54:29 -0300

    Calcular o somar e função chamando função

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,27 @@
-//console.log('Rodando via head');
+/* 
+    Objetivo: Calcular a média de 2 números
 
-let nome = 'Angelo Lustosa de Figueiredo Filho'
-let teste = nome.replace(' ', '')
-console.log(teste);
+    1 - Pegar os dois números
+    2 - Somar os dois números
+    3 - Dividir a soma pela quantidade
+*/
+
+let n1 = 100;
+let n2 = 900;
+
+function soma() {
+    console.log(n1+n2)
+    return n1+n2
+}
+
+soma()
+
+/* function media() {
+    console.log((n1+n2) / 2)
+} */
+
+function media() {
+    console.log(soma() / 2)
+}
+
+media()
