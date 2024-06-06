d731d8e9765d7656939d857f20eba30269eb4704
Angelo Lustosa de Figueiredo Filho <01337098337@DIGITALCOLLEGE.LOCAL>
2024-06-06 20:01:54 -0300

    operacaoes

--- js/script.js
+++ js/script.js
@@ -5,9 +5,69 @@ function soma() {
     let numero1 = parseFloat(document.getElementById('n1').value);
     let numero2 = parseFloat(document.getElementById('n2').value);
 
-    //
+    //pegando o resultado do caulculo e colocando na variável resultado
     let resultado = numero1 + numero2;
 
     //console.log(numero1, numero2 );
     console.log(resultado);
+
+    // Pegando o elemento HTML e colocando na variável conteudoDiv
+    const conteudoDiv = document.getElementsByClassName('resultado')[0];
+
+    // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
+    conteudoDiv.innerHTML = 'Resultado: ' + resultado
+}
+
+function subtrair() {
+    // Pegar os números dos inputs do HTML e converter para Float, pois será possível fazer o cálculo com inteiros e decimais
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    //pegando o resultado do caulculo e colocando na variável resultado
+    let resultado = numero1 - numero2;
+
+    //console.log(numero1, numero2 );
+    console.log(resultado);
+
+    // Pegando o elemento HTML e colocando na variável conteudoDiv
+    const conteudoDiv = document.getElementsByClassName('resultado')[0];
+
+    // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
+    conteudoDiv.innerHTML =  resultado
+}
+
+function multiplicacao() {
+    // Pegar os números dos inputs do HTML e converter para Float, pois será possível fazer o cálculo com inteiros e decimais
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    //pegando o resultado do caulculo e colocando na variável resultado
+    let resultado = numero1 * numero2;
+
+    //console.log(numero1, numero2 );
+    console.log(resultado);
+
+    // Pegando o elemento HTML e colocando na variável conteudoDiv
+    const conteudoDiv = document.getElementsByClassName('resultado')[0];
+
+    // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
+    conteudoDiv.innerHTML =  resultado
+}
+
+function divisao() {
+    // Pegar os números dos inputs do HTML e converter para Float, pois será possível fazer o cálculo com inteiros e decimais
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    //pegando o resultado do caulculo e colocando na variável resultado
+    let resultado = numero1 / numero2;
+
+    //console.log(numero1, numero2 );
+    console.log(resultado);
+
+    // Pegando o elemento HTML e colocando na variável conteudoDiv
+    const conteudoDiv = document.getElementsByClassName('resultado')[0];
+
+    // Pegando o conteudoDiv acessando a prop innerHTML e inserindo o valor do calculo, a variavel
+    conteudoDiv.innerHTML =  resultado
 }
